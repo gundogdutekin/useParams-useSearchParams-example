a602c4ab6e2f19b2adc0953e359d3e1fc34cd963
tekin <gundogdutekin@gmail.com>
2023-04-04 01:01:36 +0300

    project finished

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -7,17 +7,26 @@ function Members() {
   const[searchparam,setSearchParam]= useSearchParams()
   const filterBtn = () => {  
     const activeMember = searchparam.get('filter')
-    setFilter(activeMember)
+    activeMember ? setFilter(activeMember): setFilter('')
+    
   }
-  
+  const reset=()=>{
+    setSearchParam('')
+    setMembers('')
+    setFilter('')
+  }
+  const search=()=>{
+    members ? setSearchParam({filter:members}):alert('HATA!Arama yapacağınız kelimeyi girmediniz')
+  }
   return (
     <> 
     <h1>Members</h1>
     <div >
       <h3 className='danger'>{filter!=='' ? `Filtre : ${filter}`:`Arama yapacağınız kelimeyi giriniz`}</h3>
       <input className='input' type="text" value={members} onChange={(e)=>setMembers(e.target.value)} />
-      <button className='btn' onClick={()=>setSearchParam({filter:members})}>Ara</button>
+      <button className='btn' onClick={search}>Ara</button>
       <button className='btn' onClick={filterBtn}>Aktif Filtre</button>
+      <button className='btn' onClick={reset}>Temizle</button>
     </div>
     <nav className='innerLink'>
          <ul>
